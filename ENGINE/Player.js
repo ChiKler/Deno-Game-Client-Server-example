23ddce96409fa2ca3c6df08d5b85c8a4c8a02743
ChiKler <40672398+ChiKler@users.noreply.github.com>
2021-03-26 18:27:20 +0100

    Add files via upload

--- ENGINE/Player.js
+++ ENGINE/Player.js
@@ -1,3 +1,11 @@
+export var Player_WSMsg_ID;
+(function (Player_WSMsg_ID) {
+  Player_WSMsg_ID[Player_WSMsg_ID["Connection"] = 0] = "Connection";
+  Player_WSMsg_ID[Player_WSMsg_ID["Sighting"] = 1] = "Sighting";
+  Player_WSMsg_ID[Player_WSMsg_ID["Vanishing"] = 2] = "Vanishing";
+  Player_WSMsg_ID[Player_WSMsg_ID["Takedown"] = 3] = "Takedown";
+})(Player_WSMsg_ID || (Player_WSMsg_ID = {}));
+
 export class Player {
   uuID;
 
